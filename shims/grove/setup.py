a7e6158169ef8dc33f9bf670eb88557fd8838407
Jim Bennett <jim.bennett@microsoft.com>
2021-03-26 13:03:01 -0700

    Adding shim upload script

--- shims/grove/setup.py
+++ shims/grove/setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 setup(
     name='counterfit_shims_grove',
     packages=find_packages(include=['counterfit_shims_grove']),
-    version='0.1.3',
+    version='0.1.4.dev1',
     description='Shims for the Seeed Grove sensors for the CounterFit virtual IoT device app',
     long_description=long_description,
     long_description_content_type='text/markdown',

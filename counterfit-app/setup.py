dbcf513472b7ebb1e548503f72126109858abb22
Jim Bennett <jim.bennett@microsoft.com>
2021-05-17 16:31:45 -0700

    Fixing missing packages

--- counterfit-app/setup.py
+++ counterfit-app/setup.py
@@ -5,7 +5,9 @@ setup(
     install_requires=[
         "Flask==1.1.2",
         "Flask-SocketIO==5.0.1",
-        "eventlet==0.30.2"
+        "eventlet==0.30.2",
+        "beautifulsoup4==4.9.3",
+        "lxml==4.6.3"
     ],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',

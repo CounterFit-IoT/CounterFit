f8f98e8445fc9e61b11a3822324a10b9cc00f27d
Jim Bennett <jim.bennett@microsoft.com>
2022-06-21 23:53:49 +0000

    Adding Python 3.10 support

--- counterfit-app/setup.py
+++ counterfit-app/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name='CounterFit',
     install_requires=[
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
-        "eventlet==0.30.2",
+        "Flask==2.1.2",
+        "Flask-SocketIO==5.2.0",
+        "eventlet==0.33.1",
         "beautifulsoup4==4.9.3",
         "lxml==4.6.4"
     ],
